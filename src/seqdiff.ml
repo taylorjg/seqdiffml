b6a760a7a7fd43cd1de154c34414fe140a20f6ad
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-06-01 19:21:14 +0100

    diffs is now tail recursive

--- src/seqdiff.ml
+++ src/seqdiff.ml
@@ -5,21 +5,16 @@ type diff =
     | Missing of { value : int; count : int }
 
 let diffs xs =
-    let rec loop xs ev rc =
+    let rec loop xs ev rc ds =
         match xs with
-        | v :: vs when v = ev - 1 -> loop vs ev (rc + 1)
+        | v :: vs when v = ev - 1 -> loop vs ev (rc + 1) ds
         | v :: vs ->
-            let d1 = if rc > 0 then Duplicate { value = (ev - 1); count = rc } |> Some else None in
-            let d2 = if v > ev then Missing { value = ev; count = v - ev } |> Some else None in
-            let ds = loop vs (v + 1) 0 in
-            (match (d1, d2) with
-            | (Some d1, Some d2) -> d1 :: d2 :: ds
-            | (Some d1, None) -> d1 :: ds
-            | (None, Some d2) -> d2 :: ds
-            | _ -> ds)
-        | [] -> if rc > 0 then [Duplicate { value = (ev - 1); count = rc }] else []
+            let d1 = if rc > 0 then [Duplicate { value = (ev - 1); count = rc }] else [] in
+            let d2 = if v > ev then [Missing { value = ev; count = v - ev }] else [] in
+            loop vs (v + 1) 0 (ds @ d1 @ d2)
+        | [] -> ds @ if rc > 0 then [Duplicate { value = (ev - 1); count = rc }] else []
     in
-    loop xs 0 0
+    loop xs 0 0 []
 
 let string_of_diff = function
     | Duplicate { value = v; count = c } -> sprintf "Duplicate (%d, %d)" v c
